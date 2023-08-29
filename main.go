3d642dd7cec00aab0f7d5a0ff7fe85bee35336c3
aesperer <piyrw9754@gmail.com>
2023-08-29 15:48:11 +0900

    add :: go mock,
    ;

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	_ "github.com/labstack/echo/v4/middleware"
 	"goboard/handler"
+	"goboard/test"
 	"log"
+	"os"
 )
 
 func main() {
@@ -19,5 +23,9 @@ func main() {
 
 	e.POST("/api/signup", handler.SignUp)
 	e.POST("/api/signin", handler.SignIn)
+	e.GET("/api/getlist", test.MockData(), middleware.JWTWithConfig(middleware.JWTConfig{
+		SigningKey: []byte(os.Getenv("SECRET_KEY")),
+		TokenLookup: "cookie:access-token"
+	}))
 	e.Logger.Fatal(e.Start(":1323"))
 }
