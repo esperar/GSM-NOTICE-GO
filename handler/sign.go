a69f014e3756c27a95df8ace9b0e382ee8b3ae22
aesperer <piyrw9754@gmail.com>
2023-08-29 15:36:33 +0900

    add :: sign in hanlder

--- handler/sign.go
+++ handler/sign.go
@@ -38,6 +38,34 @@ func SignUp(c echo.Context) error {
 
 }
 
+func SignIn(c echo.Context) error {
+	user := new(models.User)
+
+	if err := c.Bind(user); err != nil {
+		return sendJson(http.StatusBadRequest, "bad request", c)
+	}
+
+	password := user.Password
+
+	db := database.Connect()
+	result := db.Find(user, "email=?", user.Email)
+
+	if result.RowsAffected == 0 {
+		return echo.ErrBadRequest
+	}
+
+	response := helper.CheckPasswordHash(user.Password, password)
+
+	var message string
+	if !response {
+		return echo.ErrUnauthorized
+	} else {
+		message = "Success"
+	}
+
+	return sendJson(http.StatusOK, message, c)
+}
+
 func sendJson(status int, message string, c echo.Context) error {
 	return c.JSON(status, map[string]string{
 		"message": message,
