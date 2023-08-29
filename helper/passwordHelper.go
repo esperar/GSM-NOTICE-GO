465545166932c746c952956d0ebe4a5cb97824bc
aesperer <piyrw9754@gmail.com>
2023-08-29 14:59:45 +0900

    create :: password helper

---
+++ helper/passwordHelper.go
@@ -0,0 +1,18 @@
+package helper
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(hashVal, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashVal), []byte(password))
+
+	if err != nil {
+		return false
+	} else {
+		return true
+	}
+}
